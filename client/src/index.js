59af2e77a4acdb6723b645ff7c3c2de2ddbd6fce
Zvi <zvikush90@gmail.com>
2019-12-22 00:22:34 +0200

    adapting to search

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux'
+import thunk from 'redux-thunk'
+import App from './app.js'
+import reducers from './reducer'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers(reducers)
+const store = createStore(
+  reducer,
+  compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f)
+)
+
+render( <AppContainer><Provider store={store}><App/></Provider></AppContainer>, document.getElementById("root"));
+
+if (module && module.hot) {
+  module.hot.accept('./app.js', () => {
+    const App = require('./app.js').default;
+    render(
+      <AppContainer>
+        <Provider store={store}><App/></Provider>
+      </AppContainer>,
+      document.getElementById("app")  
+    );
+  });
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
