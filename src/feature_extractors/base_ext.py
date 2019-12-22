2f3118120dad9c1c048f89d75ff99962fd51a6e6
Zvi Lapp <zvi@sagivtech.com>
2019-12-22 15:31:08 +0200

    add missing depends and metaclass adj, changed image

--- src/feature_extractors/base_ext.py
+++ src/feature_extractors/base_ext.py
@@ -1,7 +1,8 @@
 import abc
 
 
-class BaseExtractor(object, metaclass=abc.ABCMeta):
+class BaseExtractor:
+    __metaclass__=abc.ABCMeta
     @abc.abstractmethod
     def extract(self, image):
         """Extracts features from the given image using a specific class-dependent technique."""
