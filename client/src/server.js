ce808c7b934a9b244ed3d248265c0f92de3e8fc2
Zvi Lapp <zvi@sagivtech.com>
2019-12-22 08:58:58 +0200

    adding cors

--- client/src/server.js
+++ client/src/server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 module.exports = {
-  app: function () {
-    const app = express();
+  app: function () {    
+    var app = express();
+    app.use(cors());
     const indexPath = path.join(__dirname, 'index.html');
     const publicPath = express.static(path.join(__dirname, '../public'));
 
